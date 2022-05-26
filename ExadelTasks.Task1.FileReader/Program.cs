3261cda8790ce1dce82b3ced3ae28077062cc79b
Evgeniy Sinicin <catpuff13337@gmail.com>
2022-05-26 14:48:30 +0400

    Passed pathes through console args

--- ExadelTasks.Task1.FileReader/Program.cs
+++ ExadelTasks.Task1.FileReader/Program.cs
@@ -1,48 +1,46 @@
 ﻿using ExadelTasks.Task1.FileReader;
 
-while (true)
+FileMetadata? fileMeta = null;
+
+Array.ForEach(args, path =>
 {
-    var isInputPathCorrect = false;
-    FileMetadata fileMeta = null;
-
-    do
+    if (File.Exists(path))
     {
-        Console.WriteLine("Enter path to external .dll:");
-
         try
         {
-            fileMeta = new FileMetadata(Console.ReadLine());
+            fileMeta = new FileMetadata(path);
 
-            isInputPathCorrect = true;
+            Console.WriteLine($"Types count: {fileMeta?.Types.Length}\n");
+
+            foreach (var type in fileMeta.Types)
+            {
+                Console.WriteLine($"Type: {type}");
+
+                foreach (var method in FileMetadata.GetTypeMethods(type))
+                {
+                    Console.WriteLine($"Method: {method}");
+                }
+
+                foreach (var property in FileMetadata.GetTypeProperties(type))
+                {
+                    Console.WriteLine($"Property: {property}");
+                }
+
+                foreach (var member in FileMetadata.GetTypeMembers(type))
+                {
+                    Console.WriteLine($"Member: {member}");
+                }
+
+                Console.WriteLine();
+            }
         }
         catch (Exception ex)
         {
             Console.WriteLine(ex.Message);
         }
     }
-    while (!isInputPathCorrect);
-
-    Console.WriteLine($"Types count: {fileMeta.Types.Length}\n");
-
-    foreach (var type in fileMeta.Types)
+    else
     {
-        Console.WriteLine($"Type: {type}");
-
-        foreach (var method in FileMetadata.GetTypeMethods(type))
-        {
-            Console.WriteLine($"Method: {method}");
-        }
-
-        foreach (var property in FileMetadata.GetTypeProperties(type))
-        {
-            Console.WriteLine($"Property: {property}");
-        }
-
-        foreach (var member in FileMetadata.GetTypeMembers(type))
-        {
-            Console.WriteLine($"Member: {member}");
-        }
-
-        Console.WriteLine();
+        Console.WriteLine($"Path '{path}' isn't exist");
     }
-}
+});
